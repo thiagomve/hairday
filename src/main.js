8ab0e6c1ef37d1f4bb3b9855cb54b407c8dfc5fd
Thiago <thiagonfk@gmail.com>
2026-07-21 20:54:38 -0300

    feat: iniciando integração com a API

--- src/main.js
+++ src/main.js
@@ -12,3 +12,6 @@ import "./styles/schedule.css"
 import "./modules/form/submit.js"
 import "./modules/form/date-change.js"
 import "./modules/page-load.js"
+
+// import "./services/api-config.js"
+// import "./services/schedule-new.js"
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import { scheduleNew } from "../../services/schedule-new.js"
 
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
@@ -11,7 +12,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 selectedDate.value = inputToday;
 selectedDate.min = inputToday;
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault();
 
   try {
@@ -39,7 +40,7 @@ form.onsubmit = (event) => {
     // gera um ID
     const id = new Date().getTime();
 
-    console.log({
+    await scheduleNew({
       id,
       name,
       when,
